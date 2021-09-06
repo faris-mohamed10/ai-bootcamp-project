5e436b63b44664cda4962717332714ca8268a7bc
Faris Mohamed <farizmohamed1999@gmail.com>
2021-09-06 20:50:46 +0530

    minor fix

--- object_detector/main.py
+++ object_detector/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from helpers.detect import detect_image_class
@@ -15,8 +16,12 @@ def home():
 @app.route("/predict", methods=["POST"])
 def processReq():
     data = request.files["img"]
-    data.save("img.jpg")
 
-    result = detect_image_class("img.jpg")
+    save_path = os.path.join(os.path.dirname(
+        __file__), 'static/temp/test.png')
+
+    data.save(save_path)
+
+    result = detect_image_class(save_path)
 
     return render_template("result.html", result_image=result)
